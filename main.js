9cfe710e06aea14aa99019b400a2203fd08730b2
Andy McCall <andymc903@gmail.com>
2020-04-22 22:47:44 -0700

    Now logs price every five seconds

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-
-//1. Import coingecko-api
 const CoinGecko = require('coingecko-api');
-
 const BigNumber = require('bignumber.js');
-
 const  Web3 = require('web3');
-
 const contractABI = require('./erc20.abi');
+const EWMA = require('ewma');
 
 //2. Initiate the CoinGecko API Client
 const CoinGeckoClient = new CoinGecko();
@@ -14,7 +10,6 @@ const CoinGeckoClient = new CoinGecko();
 const uniswapExchange = '0x2a1530c4c41db0b0b2bb646cb5eb1a67b7158667';
 const daiAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
 const DECIMAL_PLACES = new BigNumber(10).pow(18);
-
 
 const main  = async () => {
   const web3 = new Web3(process.env['MAINNET_URL']);
@@ -43,9 +38,21 @@ const main  = async () => {
     return ethInContract.times(ethPriceBig).plus(daiInContract).div(uniSupply);
   };
 
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
 
-  const value = await getTokenValue();
-  console.log(`Value is: ${value.toNumber()}`)
+  const initialValue = await getTokenValue();
+  const FIVE_SECONDS = 5 * 1000;
+  const ewma = new EWMA(FIVE_SECONDS, initialValue.toNumber());
+  console.log('Getting token value');
+  for (let i = 0; i < 50; i++) {
+    // Sleep for n seconds
+    await sleep(FIVE_SECONDS);
+    const uniValue = await getTokenValue();
+    ewma.insert(uniValue.toNumber());
+    console.log(`Token Value: ${ewma.value()}`)
+  }
 };
 
 main().catch(error => console.error('caught an error', error));
