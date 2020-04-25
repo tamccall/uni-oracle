3c6ba0c07a1928c1e2b784aef1891efcf59ff883
Andy McCall <andymc903@gmail.com>
2020-04-25 09:03:46 -0700

    removed unneeded shutdown

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { createTerminus } = require('@godaddy/terminus');
 const http = require('http');
 
 (async () => {
-  const {subscription, ewma} = await pollPrice();
+  const {ewma} = await pollPrice();
   const app = express();
   app.get('/price', (req, res) => {
     res.send({
@@ -13,16 +13,6 @@ const http = require('http');
   });
   const server = http.createServer(app);
   createTerminus(server, {
-    onShutdown: () => {
-      console.log('unsubscribing');
-      subscription.unsubscribe(function(error, success){
-        if(success) {
-          console.log('Successfully unsubscribed!');
-        } else if (error) {
-          console.log('Error unsubscribing', error)
-        }
-      });
-    }
   });
   console.log('starting server');
   server.listen(8080);
