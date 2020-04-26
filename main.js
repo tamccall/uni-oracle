d538ca8971990f34443139879c23b6a0e340ff4b
Andy McCall <andymc903@gmail.com>
2020-04-25 18:26:43 -0700

    can publish message to skuttlebutt

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ const {pollPrice} = require('./poll');
 const express = require('express');
 const { createTerminus } = require('@godaddy/terminus');
 const http = require('http');
+const ssbClient = require('ssb-client');
 
 (async () => {
+
+  const client = await ssbClient();
+
   const {ewma} = await pollPrice((uniValue) => {
-    console.log(`Value is: ${uniValue.toFixed(18)}`)
+    console.info('have price publishing to skuttlebutt');
+    client.publish({
+      type: 'UNIV1USD',
+      price: uniValue.toNumber()
+    }, (err, msg) => {
+      if (err) {
+        console.error('error publishing to skuttlebutt', err)
+      }
+
+      console.info('successully published message to scuttlebutt', msg)
+    })
   });
+
   const app = express();
   app.get('/price', (req, res) => {
     res.send({
